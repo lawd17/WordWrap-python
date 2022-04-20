0940ac366f4a580356d6c63990ed43e21823ac0f
Jonay Hernández Izquierdo <jonayhi1717@gmail.com>
2022-04-20 13:23:04 +0100

    feat: test_return_error_for_negative_column green

--- src/Wrapper.py
+++ src/Wrapper.py
@@ -2,4 +2,7 @@ def wrap(line: str, column: int) -> str:
     if not line or line == "":
         return ""
 
+    if column < 0:
+        raise Exception("Negative columns is not allowed")
+
     return line
--- test/WrapperTest.py
+++ test/WrapperTest.py
@@ -17,10 +17,13 @@ hola mundo, 2 -> "ho\nla\nmu\nnd\no" -
 
 
 class TestIsNarcissisticNumber(TestCase):
-    def test_return_same_line_width_high_column(self):
-        line: str = "hola"
-        assert_that(wrap(line, len(line)+1)).is_equal_to(line)
-
     def test_return_emtpy_for_null_or_empty_line(self):
         assert_that(wrap("", 5)).is_equal_to("")
         assert_that(wrap(None, 5)).is_equal_to("")
+
+    def test_return_error_for_negative_column(self):
+        with self.assertRaises(Exception):
+            wrap("hello", -3)
+
+    def test_return_same_line_width_high_column(self):
+        assert_that(wrap("hello", 6)).is_equal_to("hello")
